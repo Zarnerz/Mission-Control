c3d334ecb880d4964696df9c65ca62cde054daf8
Dale Holstein <holstein30@live.com>
2019-07-06 11:42:20 -0400

    routes setup with react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/home";
+import CurrentPost from "./pages/currentPost";
+import Login from "./pages/login";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={Home} />
+        <Route path="/currentpost" component={CurrentPost} />
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
