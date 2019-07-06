fc1caec52adb32f00155544ec8f1ce2f7ae883d0
Dale Holstein <holstein30@live.com>
2019-07-06 11:37:15 -0400

    React Router added

--- frontend/src/pages/currentPost/CurrentPost.js
+++ frontend/src/pages/currentPost/CurrentPost.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Header from "../../global/header";
 import Blurb from "../../global/blurb";
 
 class CurrentPost extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Blurb title="Title" blurb="Blurby McBlurberson" />
       </div>
     );
