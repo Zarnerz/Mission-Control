d36e23b7cc9acc26f0c2ab2717d7ab9397207149
Dale Holstein <holstein30@live.com>
2019-07-06 11:28:17 -0400

    blurb added to currentPost

--- frontend/src/pages/currentPost/CurrentPost.js
+++ frontend/src/pages/currentPost/CurrentPost.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Blurb from "../../global/blurb";
 
 class CurrentPost extends Component {
   render() {
-    return <div>CurrentPost</div>;
+    return (
+      <div>
+        <Blurb title="Title" blurb="Blurby McBlurberson" />
+      </div>
+    );
   }
 }
 
